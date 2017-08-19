c1942fda9788f8eb45c095b0da30f392c7f2534c
Bogdan Cristea <cristeab@gmail.com>
2017-08-19 13:47:38 +0300

    add patch cmd

--- build-macos.sh
+++ build-macos.sh
@@ -1,3 +1,4 @@
+patch -p1 < g729_pjsua2.6.patch
 ./configure --with-opus=/usr/local/ --with-sdl=/usr/local/ --with-ssl=/usr/local/opt/openssl
 cp pjlib/include/pj/config_site_sample.h pjlib/include/pj/config_site.h
 make dep && make
